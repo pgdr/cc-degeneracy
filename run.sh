770da9758f1e8cbdb57d445bed703c6507a73bf6
Pål Grønås Drange <Pal.Drange@uib.no>
2023-05-15 14:07:22 +0200

    Update runner

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-g++ -std=c++17 -O3 c++closure.cpp && mv a.out c++closure
+echo "name,n,m,d,c,t,s,x" > run.log
+for f in ../network-corpus/networks/*gz
+do
+    echo $f
+    date
+    echo -n $f >> run.log
+    echo -n "," >> run.log
+    timeout 120s zcat $f | pypy3 degeneracy.py >> run.log
+done
 
-hyperfine --warmup=5 --output=./out.txt "zcat $1 | ./c++closure" "zcat $1 | pypy3 degeneracy.py"
+
+# g++ -std=c++17 -O3 c++closure.cpp && mv a.out c++closure
+
+# hyperfine --warmup=5 --output=./out.txt "zcat $1 | ./c++closure" "zcat $1 | pypy3 degeneracy.py"
